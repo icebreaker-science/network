bd8378f1f6de33df6660edff03ed08611cb51bb8
Chaoran Chen <mail@chaoran-chen.de>
2020-08-09 12:14:30 +0200

    Change path

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import src.data.core_data as core_data
 
 
 def main():
-    data_entries = core_data.read_from_xz('../data/00-raw/158.json.xz')
+    data_entries = core_data.read_from_xz('../data/00-raw/core_2018-03-01_fulltext/158.json.xz')
     for e in data_entries:
         pass
 
