dfdaf0aaa1bf9aa04475160aec474653d9e6b029
Chaoran Chen <mail@chaoran-chen.de>
2020-08-14 15:04:52 +0200

    Add language detection

---
+++ src/data/basic_preprocessing.py
@@ -0,0 +1,13 @@
+from langdetect import detect_langs
+
+from data.core_data import CoreDataEntry
+
+
+def detect_language(entry: CoreDataEntry) -> None:
+    s = entry.abstract or entry.title
+    if not s:
+        return
+    langs = detect_langs(s)
+    entry.language_detected_probabilities = [(lang.lang, lang.prob) for lang in langs]
+    if len(entry.language_detected_probabilities) > 0:
+        entry.language_detected_most_likely = entry.language_detected_probabilities[0][0]
--- src/main.py
+++ src/main.py
@@ -1,19 +1,35 @@
 import lzma
 from tqdm import tqdm
 
-from data import basic_filters, core_data
+from data import basic_filters, core_data, basic_preprocessing
 
 
 def main():
-    data_entries = core_data.read_all('../data/00-raw/core_2018-03-01_fulltext/')
-    chemistry_entries_with_abstracts = basic_filters.apply_filters(data_entries, [
-        basic_filters.filter_is_chemistry,
-        basic_filters.filter_has_abstract
-    ])
+    # ---------------------------
+    # Step 1: Apply basic filters
 
-    with lzma.open('../data/10-filtered/core_chemistry_with_abstracts.json.xz', mode='at') as f:
-        for entry in tqdm(chemistry_entries_with_abstracts, desc='Found entries'):
-            f.write(entry.json_raw_string)
+    # data_entries = core_data.read_all('../data/00-raw/core_2018-03-01_fulltext/')
+    # chemistry_entries_with_abstracts = basic_filters.apply_filters(data_entries, [
+    #     basic_filters.filter_is_chemistry,
+    #     basic_filters.filter_has_abstract
+    # ])
+    #
+    # with lzma.open('../data/10-filtered/core_chemistry_abstracts.json.xz', mode='at') as f:
+    #     for entry in tqdm(chemistry_entries_with_abstracts, desc='Found entries'):
+    #         f.write(entry.json_raw_string)
+
+    # ------------------------
+    # Step 2: Detect languages
+
+    data_entries = core_data.read_from_xz('../data/10-filtered/core_chemistry_abstracts.json.xz')
+    with lzma.open('../data/10-filtered/core_chemistry_abstracts_langdetected.json.xz', mode='at') as f:
+        for entry in tqdm(data_entries, desc='Processed entries'):
+            try:
+                basic_preprocessing.detect_language(entry)
+                json_str = core_data.to_json(entry) + '\n'
+                f.write(json_str)
+            except:
+                print("Unexpected error...")
 
 
 if __name__ == '__main__':
